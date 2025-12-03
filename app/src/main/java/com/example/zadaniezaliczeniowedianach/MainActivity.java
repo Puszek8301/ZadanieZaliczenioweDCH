deca959ed99598cf51e8e8b00bfb57c6f0d4eab5
diana chmiest <dianachmie@gmail.com>
2025-12-03 13:41:19 +0100

    commit5

--- app/src/main/java/com/example/zadaniezaliczeniowedianach/MainActivity.java
+++ app/src/main/java/com/example/zadaniezaliczeniowedianach/MainActivity.java
@@ -1,31 +1,43 @@
 package com.example.zadaniezaliczeniowedianach;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    EditText nameET = findViewById(R.id.name);
-    EditText lastNameET = findViewById(R.id.lastName);
-    Spinner spinner = findViewById(R.id.spinner);
-    CheckBox checkBox1 = findViewById(R.id.checkbox1);
-    CheckBox checkBox2 = findViewById(R.id.checkbox2);
-    CheckBox checkBox3 = findViewById(R.id.checkbox3);
-    Button generatePasswordBTN = findViewById(R.id.gereratePasswordBTN);
-    Button commitBTN = findViewById(R.id.commitBTN);
+    EditText nameET;
+    EditText lastNameET;
+    Spinner spinner;
+    EditText charAmountET;
+    CheckBox checkBox1;
+    CheckBox checkBox2;
+    CheckBox checkBox3;
+    Button generatePasswordBTN;
+    Button commitBTN;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        nameET = findViewById(R.id.name);
+        lastNameET = findViewById(R.id.lastName);
+        spinner = findViewById(R.id.spinner);
+        charAmountET = findViewById(R.id.charAmount);
+        checkBox1 = findViewById(R.id.checkbox1);
+        checkBox2 = findViewById(R.id.checkbox2);
+        checkBox3 = findViewById(R.id.checkbox3);
+        generatePasswordBTN = findViewById(R.id.gereratePasswordBTN);
+        commitBTN = findViewById(R.id.commitBTN);
+        String[] positions = {"Kierownik", "Starszy programista", "Młodszy programista", "Tester"};
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, positions);
+        spinner.setAdapter(spinnerAdapter);
+
 
     }
 }
