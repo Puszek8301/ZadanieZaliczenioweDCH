030bdfa35c1af79aa4fc57f760ae45ae152d3ef3
diana chmiest <dianachmie@gmail.com>
2025-12-03 13:20:52 +0100

    commit4

--- app/src/main/java/com/example/zadaniezaliczeniowedianach/MainActivity.java
+++ app/src/main/java/com/example/zadaniezaliczeniowedianach/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.zadaniezaliczeniowedianach;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText nameET = findViewById(R.id.name);
+    EditText lastNameET = findViewById(R.id.lastName);
+    Spinner spinner = findViewById(R.id.spinner);
+    CheckBox checkBox1 = findViewById(R.id.checkbox1);
+    CheckBox checkBox2 = findViewById(R.id.checkbox2);
+    CheckBox checkBox3 = findViewById(R.id.checkbox3);
+    Button generatePasswordBTN = findViewById(R.id.gereratePasswordBTN);
+    Button commitBTN = findViewById(R.id.commitBTN);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
